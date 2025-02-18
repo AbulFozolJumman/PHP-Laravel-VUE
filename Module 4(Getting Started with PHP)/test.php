6e06d16bd142720934c8c94d9e6277999b95a0ea
AbulFozolJumman <abulfozoljumman@gmail.com>
2025-02-18 23:38:39 +0600

    Getting started with PHP

---
+++ Module 4(Getting Started with PHP)/test.php
@@ -0,0 +1,24 @@
+<?php
+// ***V-2***
+// $name = "Jumman";
+// $age = 28;
+// echo $name;
+// echo "\n";
+// echo "Hello {$name} {$age}";
+
+// ***V-3***
+// define('PI',3.14159);
+// echo PI;
+// echo "Value of PI = ".PI;
+// var_dump(PI);
+
+// ***V-4***
+$planet1 = "Mercury";
+$planet2 = "Jupiter";
+// var_dump($planet1);
+// echo "The smallest planet is ".$planet1." and the biggest planet is ".$planet2."\n";
+// echo "The smallest planet is {$planet1} and the biggest planet is {$planet2}\n";
+// printf("The smallest planet is %s and the biggest planet is %s",$planet1,$planet2);
+printf("The smallest planet is %s and the biggest planet is %s", strtoupper($planet1), strrev($planet2));
+
+// ***V5***
