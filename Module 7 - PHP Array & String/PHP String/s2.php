3cc3f8a689cae39b66ea51e87bdfeeaf58d1cd45
AbulFozolJumman <abulfozoljumman@gmail.com>
2025-02-19 23:41:09 +0600

    Module 7 - PHP Array & Strings practice done

---
+++ Module 7 - PHP Array & String/PHP String/s2.php
@@ -0,0 +1,14 @@
+<?php
+$string = "Hello World";
+
+// $reverse = strrev($string);
+// echo $reverse;
+
+// echo strlen($string);
+$reverse = '';
+for ($i = strlen($string) - 1; $i >= 0; $i--) {
+    $reverse = $reverse . $string[$i];
+}
+
+echo $reverse;
+
