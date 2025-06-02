42c31751a4cd8879167c7aaa973ded4d83eb7720
AbulFozolJumman <abulfozoljumman@gmail.com>
2025-06-02 23:13:54 +0600

    Module 19 - Laravel Elequent ORM completed

--- Module 19 - Laravel Elequent ORM/practice-app/app/Models/Product.php
+++ Module 19 - Laravel Elequent ORM/practice-app/app/Models/Product.php
@@ -7,7 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    public function brand()  {
+
+    protected $hidden = ['created_at', 'updated_at']; //What you don't wanna show while retrieving
+    protected $visible = ['id', 'title', 'price']; //Only what you wanna show while retrieving
+    protected $fillable = ['title', 'price']; //Only what you wanna insert while inserting new data
+    protected $attributes = ['price'=>100]; //Setting up default value if no value given while inserting new data
+
+    function brand()  {
         return $this->belongsTo(Brand::class);
     }
 }
